0326e229c2382d57896070bf9eb3e0faf19b83a7
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-05-09 21:35:25 +0530

    Update 2-Cloud-IAM-Qwik-Start.sh

--- Track-1-Cloud-Infrastructure-Track/Quest/2.Quest-Baseline-Infrastructure/2-Cloud-IAM-Qwik-Start.sh
+++ Track-1-Cloud-Infrastructure-Track/Quest/2.Quest-Baseline-Infrastructure/2-Cloud-IAM-Qwik-Start.sh
@@ -1,21 +1,36 @@
 curl -o default.sh https://raw.githubusercontent.com/user9-21/GoogleCloudReady-Facilitator-program/main/files/default.sh
 source default.sh
 
+
 export BUCKET_NAME=$(gcloud info --format='value(config.project)')
 gsutil mb gs://$BUCKET_NAME/
-wget --output-document sample.txt https://www.cloudskillsboost.google/
+touch sample.txt
 gsutil cp sample.txt gs://$BUCKET_NAME
 
 completed "Task 1"
 
 export PROJECT_ID=$(gcloud info --format='value(config.project)')
-export LASTUSER=$(sed -E 's/MEMBERS: //gm;t;d' <<< $(gcloud projects get-iam-policy $PROJECT_ID --flatten="bindings[].members" --format='table(bindings.members)' --filter="bindings.members:user:student*" |& tail -1))
-echo $LASTUSER 
-gcloud projects remove-iam-policy-binding $PROJECT_ID --role='roles/viewer' --member $LASTUSER
+
+FIRSTUSER=$(gcloud config get-value core/account)
+LASTUSER=$(gcloud projects get-iam-policy $PROJECT_ID | grep student | awk '{print $2}' | tail -1 | sed -e 's/user://gm;t;d')
+
+if [ $FIRSTUSER = $LASTUSER ]
+then
+LASTUSER=$(gcloud projects get-iam-policy $PROJECT_ID | grep student | awk '{print $2}' | tail -2  | head -1 | sed -e 's/user://gm;t;d')
+fi
+
+if [ $FIRSTUSER = $LASTUSER ]
+then
+read -p "${YELLOW}${BOLD}Enter second Email Address : ${RESET}" LASTUSER
+echo $LASTUSER
+fi
+
+warning "Your second Email ID =${CYAN} $LASTUSER"
+gcloud projects remove-iam-policy-binding $PROJECT_ID --role='roles/viewer' --member user:$LASTUSER
 
 completed "Task 2"
-gcloud projects add-iam-policy-binding $PROJECT_ID --role='roles/storage.objectViewer' --member $LASTUSER
 
+gcloud projects add-iam-policy-binding $PROJECT_ID --role='roles/storage.objectViewer' --member $LASTUSER
 
 completed "Task 3"
 
