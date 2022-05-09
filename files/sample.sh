24127dd1d87603d0475c29f3a3499f870ff27a43
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-05-09 21:05:26 +0530

    Create sample.sh

---
+++ files/sample.sh
@@ -0,0 +1,12 @@
+curl -o default.sh https://raw.githubusercontent.com/user9-21/GoogleCloudReady-Facilitator-program/main/files/default.sh
+source default.sh
+
+
+
+completed "Task 1"
+
+completed "Task 2"
+
+completed "Lab"
+
+remove_files 
